c0ee354a519107e42664b518462bb9330d136550
Jonathan Josue Xuya Xot <jxuyax@miumg.edu.gt>
2026-09-23 16:49:57 -0600

    Configurar conexion MySQL para Railway

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,33 +1,37 @@
 /** @type {import('sequelize').Options} */
+
 module.exports = {
   development: {
     username: process.env.DEV_DB_USERNAME,
     password: process.env.DEV_DB_PASSWORD,
     database: process.env.DEV_DB_NAME,
     host: process.env.DEV_DB_HOSTNAME,
-    dialect: process.env.DEV_DB_DIALECT,
-    logging: process.env.DEV_DB_LOGGING === 'true'
-      ? console.log
-      : false,
+    dialect: process.env.DEV_DB_DIALECT || "mysql",
+    logging:
+      process.env.DEV_DB_LOGGING === "true"
+        ? console.log
+        : false,
   },
+
   test: {
     username: process.env.TEST_DB_USERNAME,
     password: process.env.TEST_DB_PASSWORD,
     database: process.env.TEST_DB_NAME,
     host: process.env.TEST_DB_HOSTNAME,
-    dialect: process.env.TEST_DB_DIALECT,
-    logging: process.env.TEST_DB_LOGGING === 'true'
-      ? console.log
-      : false,
+    dialect: process.env.TEST_DB_DIALECT || "mysql",
+    logging:
+      process.env.TEST_DB_LOGGING === "true"
+        ? console.log
+        : false,
   },
+
   production: {
-    username: process.env.PROD_DB_USERNAME,
+    username: process.env.PROD_DB_USER,
     password: process.env.PROD_DB_PASSWORD,
     database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    dialect: process.env.PROD_DB_DIALECT,
-    logging: process.env.PROD_DB_LOGGING === 'true'
-      ? console.log
-      : false,
+    host: process.env.PROD_DB_HOST,
+    port: process.env.PROD_DB_PORT || 3306,
+    dialect: "mysql",
+    logging: false,
   },
 };
