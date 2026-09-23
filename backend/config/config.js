8941d1fb253162a72bdea17f8f992ab3f82fd968
Jonathan Josue Xuya Xot <jxuyax@miumg.edu.gt>
2026-09-23 16:27:46 -0600

    Configuracion del proyecto Conduit para QA y despliegue

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,7 +6,9 @@ module.exports = {
     database: process.env.DEV_DB_NAME,
     host: process.env.DEV_DB_HOSTNAME,
     dialect: process.env.DEV_DB_DIALECT,
-    logging: process.env.DEV_DB_LOGGING,
+    logging: process.env.DEV_DB_LOGGING === 'true'
+      ? console.log
+      : false,
   },
   test: {
     username: process.env.TEST_DB_USERNAME,
@@ -14,7 +16,9 @@ module.exports = {
     database: process.env.TEST_DB_NAME,
     host: process.env.TEST_DB_HOSTNAME,
     dialect: process.env.TEST_DB_DIALECT,
-    logging: process.env.TEST_DB_LOGGING,
+    logging: process.env.TEST_DB_LOGGING === 'true'
+      ? console.log
+      : false,
   },
   production: {
     username: process.env.PROD_DB_USERNAME,
@@ -22,6 +26,8 @@ module.exports = {
     database: process.env.PROD_DB_NAME,
     host: process.env.PROD_DB_HOSTNAME,
     dialect: process.env.PROD_DB_DIALECT,
-    logging: process.env.PROD_DB_LOGGING,
+    logging: process.env.PROD_DB_LOGGING === 'true'
+      ? console.log
+      : false,
   },
 };
