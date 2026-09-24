06e43c78182f869d0f7f0e76f603de973514057b
Jonathan Josue Xuya Xot <jxuyax@miumg.edu.gt>
2026-09-23 18:06:29 -0600

    Configurar conexion del frontend con backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./axiosConfig";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
@@ -47,14 +48,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
               <Route path="favorites" element={<ProfileFavArticles />} />
             </Route>
           </Route>
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </AuthProvider>
     </HashRouter>
-  </React.StrictMode>,
-  );
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint.
 reportWebVitals();
